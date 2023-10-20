7b42c402c86438da95ea07f8a324ceceff487987
thetahayaseen <thetahayaseen>
2023-10-20 02:37:42 -0700

    Basic Flutter Notes

---
+++ Notes.py
@@ -0,0 +1,16 @@
+"""
+
+Flutter Depends On Widgets
+    
+    Root Widget
+        Some Widgets Inside The Root Widget
+            Sub Widgets In Some Widgets
+                The Hierarchy Goes On.....
+
+    There Are Many Pre-Defined Widgets In Flutter
+        Such As Conainer Widgets Or Graphical Widgets Like Text etc...
+        These Widgets Also Have Attributes Of Their Own Characteriing Them Even More Like Text Widget Has Color Attribute
+
+    These Widgets Are Just Defined Classes In Dart Making Up A Tree (Being Nested In Each Other) At The End...
+
+"""

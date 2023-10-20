7b6e6e634c5d102456ae717184f42ea0bbe97455
thetahayaseen <thetahayaseen>
2023-10-20 02:42:13 -0700

    Variables

--- DartPrime.dart
+++ DartPrime.dart
@@ -1,3 +1,16 @@
 void main() {
   print("hello, world!");
+
+  // statically typed language
+
+  // datatype variableName = variableValue;
+
+  String name = "The Taha Yaseen";
+  int age = 14;
+  bool isProgrammer = true;
+
+  // age = "15"; Cannot Change Types
+
+  dynamic experienceOfProgrammingInYears = 3;
+  experienceOfProgrammingInYears = "3"; // Allows Changing Types
 }

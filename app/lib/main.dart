984c8d97b77b6eff7ef10fc602f198253e9bdc7f
The Taha Yaseen <thetahayaseen@gmail.com>
2023-10-24 04:13:19 +0500

    Introduced Style!

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -40,7 +40,15 @@ class MainScreen extends StatelessWidget {
       // Containers Can Only Have One Child Specified By Child:
       // Center Is A Container With A Child Centering Its Child
       body: const Center(
-        child: Text("hello, world!"),
+        child: Text(
+          "hello, world!",
+          // Style Is An Attribute Particular For A Widget Allowing Us To Style It
+          style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+              color: Colors.blueGrey),
+        ),
       ),
       // A Button That Floats In The Scaffold
       // OnPressed: Takes A Function To Execute When User Presses The Button
